ce577ef69374c377e7042e044ef8e36171e11cb7
Ruairi Tierney <ruairitierney212@gmail.com>
2024-11-06 16:21:46 +0000

    updated dashboard GUI

--- Form7.Designer.cs
+++ Form7.Designer.cs
@@ -33,59 +33,110 @@
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
             this.button5 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
             this.SuspendLayout();
             // 
             // button1
             // 
+            this.button1.BackColor = System.Drawing.Color.BlanchedAlmond;
             this.button1.Location = new System.Drawing.Point(305, 6);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(93, 29);
             this.button1.TabIndex = 0;
             this.button1.Text = "Today";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.UseVisualStyleBackColor = false;
             // 
             // button2
             // 
+            this.button2.BackColor = System.Drawing.Color.BlanchedAlmond;
             this.button2.Location = new System.Drawing.Point(404, 6);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(93, 29);
             this.button2.TabIndex = 1;
             this.button2.Text = "Last 7 days";
-            this.button2.UseVisualStyleBackColor = true;
+            this.button2.UseVisualStyleBackColor = false;
             // 
             // button3
             // 
+            this.button3.BackColor = System.Drawing.Color.BlanchedAlmond;
             this.button3.Location = new System.Drawing.Point(503, 6);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(93, 29);
             this.button3.TabIndex = 2;
             this.button3.Text = "last month";
-            this.button3.UseVisualStyleBackColor = true;
+            this.button3.UseVisualStyleBackColor = false;
             // 
             // button4
             // 
+            this.button4.BackColor = System.Drawing.Color.BlanchedAlmond;
             this.button4.Location = new System.Drawing.Point(602, 6);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(93, 29);
             this.button4.TabIndex = 3;
             this.button4.Text = "last 3 months";
-            this.button4.UseVisualStyleBackColor = true;
+            this.button4.UseVisualStyleBackColor = false;
             // 
             // button5
             // 
+            this.button5.BackColor = System.Drawing.Color.BlanchedAlmond;
             this.button5.Location = new System.Drawing.Point(701, 6);
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(93, 29);
             this.button5.TabIndex = 4;
             this.button5.Text = "last year";
-            this.button5.UseVisualStyleBackColor = true;
+            this.button5.UseVisualStyleBackColor = false;
             this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Bisque;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(12, 63);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(109, 61);
+            this.panel1.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "TOTALS:";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Bisque;
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Location = new System.Drawing.Point(388, 129);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(109, 61);
+            this.panel2.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(111, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Total events attended";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
             // MemberInsightDashboard
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Tan;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.button5);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
@@ -93,6 +144,10 @@
             this.Controls.Add(this.button1);
             this.Name = "MemberInsightDashboard";
             this.Text = "MemberInsightDashboard";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -104,5 +159,9 @@
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label2;
     }
 }
--- Form7.cs
+++ Form7.cs
@@ -21,5 +21,10 @@ namespace member_space
         {
 
         }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
