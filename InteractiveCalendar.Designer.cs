0535954e5f55148db15a50e05a3bfef4c1479199
Ruairi Tierney <ruairitierney212@gmail.com>
2024-11-15 15:09:54 +0000

    Interactive calendar automatic panel creation

--- InteractiveCalendar.Designer.cs
+++ InteractiveCalendar.Designer.cs
@@ -28,160 +28,45 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.Monday = new System.Windows.Forms.Label();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
-            this.label7 = new System.Windows.Forms.Label();
-            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
-            this.panel2 = new System.Windows.Forms.Panel();
-            this.flowLayoutPanel1.SuspendLayout();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.Calendar = new System.Windows.Forms.Panel();
             this.SuspendLayout();
             // 
-            // Monday
+            // panel1
             // 
-            this.Monday.AutoSize = true;
-            this.Monday.BackColor = System.Drawing.Color.LightCoral;
-            this.Monday.Font = new System.Drawing.Font("Rockwell", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Monday.Location = new System.Drawing.Point(41, 92);
-            this.Monday.Name = "Monday";
-            this.Monday.Size = new System.Drawing.Size(69, 19);
-            this.Monday.TabIndex = 0;
-            this.Monday.Text = "Monday";
-            this.Monday.Click += new System.EventHandler(this.label1_Click);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1151, 100);
+            this.panel1.TabIndex = 1;
             // 
-            // label1
+            // Calendar
             // 
-            this.label1.AutoSize = true;
-            this.label1.BackColor = System.Drawing.Color.LightCoral;
-            this.label1.Font = new System.Drawing.Font("Rockwell", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(194, 92);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(71, 19);
-            this.label1.TabIndex = 8;
-            this.label1.Text = "Tuesday";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.BackColor = System.Drawing.Color.LightCoral;
-            this.label2.Font = new System.Drawing.Font("Rockwell", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(662, 92);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(57, 19);
-            this.label2.TabIndex = 9;
-            this.label2.Text = "Friday";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.BackColor = System.Drawing.Color.LightCoral;
-            this.label3.Font = new System.Drawing.Font("Rockwell", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(340, 92);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(97, 19);
-            this.label3.TabIndex = 9;
-            this.label3.Text = "Wednesday";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.BackColor = System.Drawing.Color.LightCoral;
-            this.label4.Font = new System.Drawing.Font("Rockwell", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(498, 92);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(78, 19);
-            this.label4.TabIndex = 10;
-            this.label4.Text = "Thursday";
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.BackColor = System.Drawing.Color.LightCoral;
-            this.label5.Font = new System.Drawing.Font("Rockwell", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(789, 92);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(76, 19);
-            this.label5.TabIndex = 11;
-            this.label5.Text = "Saturday";
-            // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.BackColor = System.Drawing.Color.LightCoral;
-            this.label6.Font = new System.Drawing.Font("Rockwell", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(956, 92);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(64, 19);
-            this.label6.TabIndex = 12;
-            this.label6.Text = "Sunday";
-            // 
-            // label7
-            // 
-            this.label7.AutoSize = true;
-            this.label7.BackColor = System.Drawing.Color.LightCoral;
-            this.label7.Font = new System.Drawing.Font("Rockwell", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label7.Location = new System.Drawing.Point(31, 27);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(113, 31);
-            this.label7.TabIndex = 13;
-            this.label7.Text = "MONTH";
-            this.label7.Click += new System.EventHandler(this.label7_Click);
-            // 
-            // flowLayoutPanel1
-            // 
-            this.flowLayoutPanel1.BackColor = System.Drawing.Color.WhiteSmoke;
-            this.flowLayoutPanel1.Controls.Add(this.panel2);
-            this.flowLayoutPanel1.Location = new System.Drawing.Point(2, 125);
-            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
-            this.flowLayoutPanel1.Size = new System.Drawing.Size(1062, 430);
-            this.flowLayoutPanel1.TabIndex = 7;
-            // 
-            // panel2
-            // 
-            this.panel2.Location = new System.Drawing.Point(3, 3);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(145, 100);
-            this.panel2.TabIndex = 1;
+            this.Calendar.BackColor = System.Drawing.Color.Maroon;
+            this.Calendar.Location = new System.Drawing.Point(0, 106);
+            this.Calendar.Name = "Calendar";
+            this.Calendar.Size = new System.Drawing.Size(1142, 527);
+            this.Calendar.TabIndex = 0;
+            this.Calendar.Paint += new System.Windows.Forms.PaintEventHandler(this.Calendar_Paint);
             // 
             // InteractiveCalendar
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Red;
-            this.ClientSize = new System.Drawing.Size(1064, 554);
-            this.Controls.Add(this.label7);
-            this.Controls.Add(this.label6);
-            this.Controls.Add(this.label5);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.Monday);
-            this.Controls.Add(this.flowLayoutPanel1);
+            this.ClientSize = new System.Drawing.Size(1151, 648);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.Calendar);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "InteractiveCalendar";
             this.Text = "Form14";
             this.Load += new System.EventHandler(this.InteractiveCalendar_Load);
-            this.flowLayoutPanel1.ResumeLayout(false);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Label Monday;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.Label label7;
-        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
-        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel Calendar;
     }
 }
--- InteractiveCalendar.cs
+++ InteractiveCalendar.cs
@@ -15,11 +15,44 @@ namespace member_space
         public InteractiveCalendar()
         {
             InitializeComponent();
+            calendar_creator(DateTime.Now.Year, DateTime.Now.Month);
         }
+        private void calendar_creator(int year, int month)
+        {
+            //This will clear the previous controls
+            Calendar.Controls.Clear();
 
+            //This will get the first day of the month so we can create the ordering of the panels off of it
+            DateTime month_Start = new DateTime(year, month, 1);
+
+            //This will calculate the days in the current month to get number of panels needed
+            int days_Month = DateTime.DaysInMonth(year, month);
+
+            //This gets the day of the week of the first date
+            int first_Day = (int)month_Start.DayOfWeek;
+            //This is the panel size from testing
+            int panel_Width = 156;
+            int panel_Height = 100;
+           
+            //This will loop through the days in the month creating a panel for each
+            for (int i = 1; i <= days_Month; i++) {
+                //Calc the rows needed
+                int col = (first_Day + i - 1) % 7;
+                int row = (first_Day + i - 1) / 7;
+                
+                Panel day_Panel = new Panel
+                {
+                    Size = new Size(panel_Width - 5, panel_Height - 5),
+                    Location = new Point(col * panel_Width, row * panel_Height),
+                    BorderStyle = BorderStyle.FixedSingle,
+                    BackColor = Color.IndianRed
+                };
+            Calendar.Controls.Add(day_Panel);
+            
+            }
+        }
         private void label1_Click(object sender, EventArgs e)
         {
-
         }
 
         private void label7_Click(object sender, EventArgs e)
@@ -28,6 +61,26 @@ namespace member_space
         }
 
         private void InteractiveCalendar_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void Calendar_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void panel5_Paint(object sender, PaintEventArgs e)
         {
 
         }
