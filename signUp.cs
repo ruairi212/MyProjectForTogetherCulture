bc35dd04a1b96e71a33170d4edf1cf453aa43fd1
diyakapoorr <diyakapoor2004@gmail.com>
2024-11-15 00:45:37 +0000

    register

---
+++ signUp.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace member_space
+{
+    public partial class signUp : Form
+    { 
+
+         
+        public signUp()
+        {
+            InitializeComponent();
+        }
+
+        private void signUp_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello, World!");
+        }
+    }
+}
