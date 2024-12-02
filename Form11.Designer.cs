e3538028824d2304f05e4564e07024a7b3d4dfb4
Dheeraj Kodwani <dheerajkodwani21@gmail.com>
2024-12-02 13:18:16 +0000

    made chnages to know more button, where upon loading it will show admin info and made the login/signup button also clickable

--- Form11.Designer.cs
+++ Form11.Designer.cs
@@ -41,10 +41,12 @@
             this.panel3 = new System.Windows.Forms.Panel();
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.button2 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.panel2.SuspendLayout();
             this.panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
@@ -59,20 +61,21 @@
             this.panel1.Location = new System.Drawing.Point(2, 2);
             this.panel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(708, 80);
+            this.panel1.Size = new System.Drawing.Size(1392, 100);
             this.panel1.TabIndex = 1;
             // 
             // login
             // 
             this.login.AutoSize = true;
             this.login.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
-            this.login.Location = new System.Drawing.Point(590, 27);
+            this.login.Location = new System.Drawing.Point(1186, 30);
             this.login.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.login.Name = "login";
-            this.login.Size = new System.Drawing.Size(117, 42);
+            this.login.Size = new System.Drawing.Size(132, 62);
             this.login.TabIndex = 4;
             this.login.Text = "Login/\r\nsignup";
             this.login.UseVisualStyleBackColor = false;
+            this.login.Click += new System.EventHandler(this.login_Click);
             // 
             // pictureBox1
             // 
@@ -80,7 +83,7 @@
             this.pictureBox1.Location = new System.Drawing.Point(0, 0);
             this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(169, 80);
+            this.pictureBox1.Size = new System.Drawing.Size(190, 100);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
             this.pictureBox1.TabIndex = 5;
             this.pictureBox1.TabStop = false;
@@ -88,10 +91,10 @@
             // blogButton
             // 
             this.blogButton.AutoSize = true;
-            this.blogButton.Location = new System.Drawing.Point(504, 29);
+            this.blogButton.Location = new System.Drawing.Point(966, 38);
             this.blogButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.blogButton.Name = "blogButton";
-            this.blogButton.Size = new System.Drawing.Size(85, 37);
+            this.blogButton.Size = new System.Drawing.Size(96, 46);
             this.blogButton.TabIndex = 3;
             this.blogButton.Text = "BLOG";
             this.blogButton.UseVisualStyleBackColor = true;
@@ -100,10 +103,10 @@
             // shopButton
             // 
             this.shopButton.AutoSize = true;
-            this.shopButton.Location = new System.Drawing.Point(429, 30);
+            this.shopButton.Location = new System.Drawing.Point(744, 38);
             this.shopButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.shopButton.Name = "shopButton";
-            this.shopButton.Size = new System.Drawing.Size(85, 37);
+            this.shopButton.Size = new System.Drawing.Size(96, 46);
             this.shopButton.TabIndex = 2;
             this.shopButton.Text = "SHOP";
             this.shopButton.UseVisualStyleBackColor = true;
@@ -112,10 +115,10 @@
             // whatsonButton
             // 
             this.whatsonButton.AutoSize = true;
-            this.whatsonButton.Location = new System.Drawing.Point(290, 30);
+            this.whatsonButton.Location = new System.Drawing.Point(458, 38);
             this.whatsonButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.whatsonButton.Name = "whatsonButton";
-            this.whatsonButton.Size = new System.Drawing.Size(142, 37);
+            this.whatsonButton.Size = new System.Drawing.Size(160, 46);
             this.whatsonButton.TabIndex = 1;
             this.whatsonButton.Text = "WHAT\'S ON";
             this.whatsonButton.UseVisualStyleBackColor = true;
@@ -124,10 +127,10 @@
             // aboutButton
             // 
             this.aboutButton.AutoSize = true;
-            this.aboutButton.Location = new System.Drawing.Point(196, 30);
+            this.aboutButton.Location = new System.Drawing.Point(220, 38);
             this.aboutButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.aboutButton.Name = "aboutButton";
-            this.aboutButton.Size = new System.Drawing.Size(99, 37);
+            this.aboutButton.Size = new System.Drawing.Size(111, 46);
             this.aboutButton.TabIndex = 0;
             this.aboutButton.Text = "ABOUT";
             this.aboutButton.UseVisualStyleBackColor = true;
@@ -135,21 +138,23 @@
             // 
             // panel2
             // 
+            this.panel2.Controls.Add(this.dataGridView1);
             this.panel2.Controls.Add(this.button1);
-            this.panel2.Location = new System.Drawing.Point(418, 82);
+            this.panel2.Location = new System.Drawing.Point(470, 102);
             this.panel2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(292, 261);
+            this.panel2.Size = new System.Drawing.Size(924, 326);
             this.panel2.TabIndex = 2;
+            this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
             // 
             // button1
             // 
             this.button1.AutoSize = true;
             this.button1.BackColor = System.Drawing.Color.CornflowerBlue;
-            this.button1.Location = new System.Drawing.Point(88, 5);
+            this.button1.Location = new System.Drawing.Point(384, 6);
             this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(162, 37);
+            this.button1.Size = new System.Drawing.Size(182, 46);
             this.button1.TabIndex = 0;
             this.button1.Text = "Administrators";
             this.button1.UseVisualStyleBackColor = false;
@@ -158,18 +163,18 @@
             // 
             this.panel3.Controls.Add(this.richTextBox1);
             this.panel3.Controls.Add(this.button2);
-            this.panel3.Location = new System.Drawing.Point(2, 82);
+            this.panel3.Location = new System.Drawing.Point(2, 102);
             this.panel3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(418, 261);
+            this.panel3.Size = new System.Drawing.Size(470, 326);
             this.panel3.TabIndex = 3;
             // 
             // richTextBox1
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(3, 59);
+            this.richTextBox1.Location = new System.Drawing.Point(3, 74);
             this.richTextBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(415, 200);
+            this.richTextBox1.Size = new System.Drawing.Size(466, 249);
             this.richTextBox1.TabIndex = 1;
             this.richTextBox1.Text = resources.GetString("richTextBox1.Text");
             // 
@@ -177,20 +182,31 @@
             // 
             this.button2.AutoSize = true;
             this.button2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button2.BackgroundImage")));
-            this.button2.Location = new System.Drawing.Point(3, 5);
+            this.button2.Location = new System.Drawing.Point(3, 6);
             this.button2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(441, 86);
+            this.button2.Size = new System.Drawing.Size(496, 108);
             this.button2.TabIndex = 0;
             this.button2.Text = "Together Culture is excited to announce the \r\nlaunch of \'Let’s Go Fly The Kite\',\r" +
     "\n funded by the Heritage Fund. ";
             this.button2.UseVisualStyleBackColor = true;
             // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(7, 57);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(866, 266);
+            this.dataGridView1.TabIndex = 4;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
             // Form11
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(712, 338);
+            this.ClientSize = new System.Drawing.Size(1344, 422);
             this.Controls.Add(this.panel3);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
@@ -205,6 +221,7 @@
             this.panel2.PerformLayout();
             this.panel3.ResumeLayout(false);
             this.panel3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -223,5 +240,6 @@
         private System.Windows.Forms.Panel panel3;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
