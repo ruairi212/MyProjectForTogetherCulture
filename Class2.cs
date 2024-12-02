2f6d0489caf05b217095ecf83854f453fe4d0d1a
Ruairi Tierney <ruairitierney212@gmail.com>
2024-12-02 12:27:33 +0000

    trying to add functionality

---
+++ Class2.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace member_space
+{
+    public class Search_Members
+    {
+        DataTable Search_Result = new DataTable();
+        string query;
+        
+        
+    }
+}
